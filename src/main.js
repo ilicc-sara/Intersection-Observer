b410e7ae2472527c15afde80bba5233d4d80864c
Sara <sarailic431@gmail.com>
2025-03-08 16:28:33 +0100

    intersection observer

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
+"use strict";
 import "./style.css";
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+    console.log(entries);
+  },
+  {
+    threshold: 1,
+  }
+);
+
+// observer.observe(cards[0]);
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
